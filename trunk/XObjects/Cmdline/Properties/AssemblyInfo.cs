bba937180fc4ba2637ff85349b8b2984f5b6965b
SND\sergey_shandar_cp <SND\sergey_shandar_cp@a83551a4-30f6-4d81-a974-c6ced450ddbf>
2010-03-02 04:28:26 +0000

    Now the projects require generated _info.cs files. Use a build tool in the folder "build":
    WARNING: You must run the tool before compiling other projects. 
    WARNING: The tool requires c:\program files\7-Zip\7z.exe and it uses c:\bin\Debug c:\bin\linqtoxsd.*.*.*.*-bin as temporary folders!
    
    
    git-svn-id: https://linqtoxsd.svn.codeplex.com/svn@40165 a83551a4-30f6-4d81-a974-c6ced450ddbf

--- trunk/XObjects/Cmdline/Properties/AssemblyInfo.cs
+++ trunk/XObjects/Cmdline/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LINQ to XSD Command Line")]
+// [assembly: AssemblyTitle("LINQ to XSD Command Line")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("LINQ to XSD")]
-[assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation.  All rights reserved.")]
+// [assembly: AssemblyProduct("LINQ to XSD")]
+// [assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation.  All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("1.0.0.0")]
+// [assembly: AssemblyFileVersion("1.0.0.0")]
--- trunk/XObjects/XOTask/Properties/AssemblyInfo.cs
+++ trunk/XObjects/XOTask/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XOTask")]
+// [assembly: AssemblyTitle("XOTask")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("XOTask")]
-[assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation.  All rights reserved.")]
+// [assembly: AssemblyProduct("XOTask")]
+// [assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation.  All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("1.0.0.0")]
+// [assembly: AssemblyFileVersion("1.0.0.0")]
