d5c499aaa38fabe76300b88d38b6cd0b7c473e02
SND\sergey_shandar_cp <SND\sergey_shandar_cp@a83551a4-30f6-4d81-a974-c6ced450ddbf>
2010-09-16 13:44:41 +0000

    union test.
    git-svn-id: https://linqtoxsd.svn.codeplex.com/svn@55760 a83551a4-30f6-4d81-a974-c6ced450ddbf

--- trunk/Xml.Schema.Linq.Xunit/Attribute.cs
+++ trunk/Xml.Schema.Linq.Xunit/Attribute.cs
@@ -5,6 +5,7 @@
     using XML = global::System.Xml;
     using A = global::System.Reflection.Assembly;
     using G = XObjectsGenerator.XObjectsGenerator;
+    using R = Properties.Resources;
 
     public class Attribute
     {
@@ -16,19 +17,37 @@
             G.GenerateXObjects(set, null, null, null, true, false);
         }
 
-        [X.Fact]
-        public void Union()
+        public void UnionTest(string s)
         {
             var set = new S.XmlSchemaSet();
             var settings = new XML.XmlReaderSettings()
             {
                 DtdProcessing = XML.DtdProcessing.Parse,
             };
-            var reader = new System.IO.StringReader(Properties.Resources.UnionTest);
+            var reader = new System.IO.StringReader(s);
             var r = XML.XmlReader.Create(reader, settings);
             set.Add(null, r);
             G.ThisAssembly = A.GetExecutingAssembly();
             G.GenerateXObjects(set, null, null, null, true, false);
         }
+
+        [X.Fact]
+        public void Union()
+        {
+            this.UnionTest(R.UnionTest);
+        }
+
+        [X.Fact]
+        public void UnionBasic()
+        {
+            this.UnionTest(R.UnionTestBasic);
+        }
+
+        [X.Fact]
+        public void UnionType()
+        {
+            this.UnionTest(R.UnionTestType);
+        }
+
     }
 }
